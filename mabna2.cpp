18fd3909014e7e583f818bdd811b4a73c8d43bba
Abolfazlkhademian <161434250+Abolfazlkhademian@users.noreply.github.com>
2024-05-26 11:19:47 +0330

    mabna2.cpp

--- mabna2.cpp
+++ mabna2.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+using namespace std;
+
+void Binary(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    else
+    {
+        Binary(n / 2);
+        cout << n % 2;
+    }
+}
 
 int main()
 {
-    int x;
-    std::cin >> x;
-    while (x / 2 != 0)
-    {
-        std::cout << x % 2;
-        x = x / 2;
-    }
-    std::cout << x % 2;
+    int Number;
+
+    cout << "Enter a number: ";
+    cin >> Number;
+
+    cout << "Binary number is : ";
+    Binary(Number);
+
+    return 0;
 }
